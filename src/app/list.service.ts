5a2369859af4470f558f05e8467a51167abff82f
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2023-05-31 18:45:47 +0300

    Added list service

---
+++ src/app/list.service.ts
@@ -0,0 +1,29 @@
+export class ListService {
+  list = [];
+
+  getList() {
+    return [...this.list];
+  }
+
+  addItem(item) {
+    // TODO add item
+  }
+  removeItem(id) {
+    // TODO remove item
+  }
+  editItem() {
+    // TODO edit item
+  }
+  toggleCheckItem() {
+    // TODO toggle check item
+  }
+  checkAll() {
+    // TODO check all
+  }
+  uncheckAll() {
+    // TODO uncheck all
+  }
+  removeAll() {
+    // TODO remove all
+  }
+}
